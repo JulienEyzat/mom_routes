2aa901055c9e1b652cf5225522ed19f139f0fe86
Julien Eyzat <julien.eyzat@telecomnancy.eu>
2019-03-21 13:54:42 +0100

    add desc

--- json2kml.py
+++ json2kml.py
@@ -5,27 +5,45 @@ with open('initiatives_all.json', 'r') as file:
     initiatives_json = file.read()
 
 dict_initiatives = json.loads(initiatives_json)
+
 initiatives_kml = simplekml.Kml()
 
-
 themes = set()
+all_titles = set()
+dict_title_coord = {}
+dict_title_desc = {}
+nb_init = 0
 for region in range(len(dict_initiatives)):
     for id in dict_initiatives[region]:
         # print(dict_initiatives[region][id]['theme'])
-        themes.add(dict_initiatives[region][id]['theme'])
-        if dict_initiatives[region][id]['theme'] in ['Déchets', 'Energies renouvelables']:
-            if 'méthan' in dict_initiatives[region][id]['title'].lower():
-                title = dict_initiatives[region][id]['title']
-                latitude = dict_initiatives[region][id]['location']['latitude']
-                longitude = dict_initiatives[region][id]['location']['longitude']
-                if title == "Méthanisation VIN Cave Coopérative La Courtoise":
-                    print(latitude)
-                    print(longitude)
-                initiatives_kml.newpoint(name=title, coords=[(longitude,latitude)])
+        initiative = dict_initiatives[region][id]
+        themes.add(initiative['theme'])
+        if initiative['theme'] in ['Déchets', 'Energies renouvelables']:
+            if 'méthan' in initiative['title'].lower():
+                title = initiative['title']
+                latitude = initiative['location']['latitude']
+                longitude = initiative['location']['longitude']
+                # print(initiative['body'])
+                dict_title_coord[title] = (longitude, latitude)
+                dict_title_desc[title] = initiative['body']
+                # if title == "Méthanisation IAA Fromagerie Gaugry":
+                #     print(latitude)
+                #     print(longitude)
+                #     print(initiative['body'])
+                #     print(initiative['location'])
+                if not title in all_titles:
+                    all_titles.add(title)
+                    nb_init += 1
                 #long, lat
 # Déchets, Energies renouvelables
 
+for initiative in dict_title_coord:
+    pnt = initiatives_kml.newpoint(name=initiative, description=dict_title_desc[initiative], coords=[dict_title_coord[initiative]])
+    pnt.style.labelstyle.scale = 0.0001  # Text twice as big
+    # pnt.style.labelstyle.color = simplekml.Color.blue
 
+
+print(nb_init)
 
 initiatives_kml.save('initiatives.kml')
 print(themes)
